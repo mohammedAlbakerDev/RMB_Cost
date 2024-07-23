233972a70b34aca83e376b4d4424567a76cabeb9
mohammedAlbakerDev <mohammedismaelalbaker@gmail.com>
2024-07-23 20:48:55 +0300

    'pwa'

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('static-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/calcIcon.png',
+'/calcIcon2.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
