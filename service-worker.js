ce858df4a3a3c880b72f36e5a4158597c8fe9f8f
mohammedAlbakerDev <139398573+mohammedAlbakerDev@users.noreply.github.com>
2024-07-24 11:02:59 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('static-cache').then(function(cache) {
       return cache.addAll([
-        '/',
         '/index.html',
         '/manifest.json',
         '/calcIcon.png',
